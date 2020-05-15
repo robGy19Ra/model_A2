d9b5742fc6103680c45a977d76611ff507300f3d
robGy19Ra <62848502+robGy19Ra@users.noreply.github.com>
2020-05-15 04:45:50 +0100

    update

--- framework.py
+++ framework.py
@@ -8,24 +8,30 @@ import math
 import tkinter
 import tkinter.filedialog
 import csv
+import json
+import requests
+import bs4
 
 
 class gradient():
-  
+    
+    #calculate maximum downhill gradient across elevation dataset
     def angle(elevation, cell_distance, gradient):
+        #n is a reference for each row across the 2d array
         n = 0
         for e_row in elevation:
             g_row = []
+            #i is reference for each value in row
             i = 0
             for e_value in e_row:
                 """if statements stop the script looking over the edges of the raster
                 and sets slope from off the map to 0"""
                 if i == len(e_row)-1:     #if i is the final figure on a row
-                    i_add = i           #figure to right is given value of i
+                    i_add = i           #imaginary figure to right is given value of i
                     i_sbtr = i-1        #figure to left can still be found
                     
                 elif i == 0:            #if i is the first figure on a row    
-                    i_sbtr = i          #figure to left is given the value of i
+                    i_sbtr = i          #imaginary figure to left is given the value of i
                     i_add = i+1         #figure to right can still be found
                     
                 else:                   #if i is not first or last position of row
@@ -33,18 +39,18 @@ class gradient():
                     i_sbtr = i-1        #figure to left can be found
                     
                 if n == len(elevation)-1:   #if n is the final row in the dataset
-                    n_add = n               #figure below is given the value of n
+                    n_add = n               #imaginary figure below is given the value of n
                     n_sbtr = n-1            #figure above can still be found
                     
                 elif n == 0:                #if n is the first row in the dataset
-                    n_sbtr = n              #figure above is given the value of n
+                    n_sbtr = n              #imaginary figure above is given the value of n
                     n_add = n+1             #figure below can still be found
                     
                 else:                   #if n i not in the first or last row in dataset
                     n_add = n+1         #figure below can still be found
                     n_sbtr = n-1        #figure above can still be found
                     
-            #finding the value of surrounding elevations in R8 form
+                #finding the value of surrounding elevations in R8 form
                 g1 = (elevation[n][i_add]) 
                 g2 = (elevation[n_sbtr][i_add])
                 g3 = (elevation[n_sbtr][i])
@@ -54,48 +60,80 @@ class gradient():
                 g7 = (elevation[n_add][i])
                 g8 = (elevation[n_add][i_add])
         
-                r8 = [g1, g2, g3, g4, g5, g6, g7, g8]
-             
-                height_difference = e_value - min(r8)
+                #diagonally located reference cells
+                r8_diag = [g2, g4, g6, g8]
+                #perpendicular located reference cells
+                r8_xy = [g1, g3, g5, g7]
+                
+                #finding the lowest elevation from each set
+                min_r8_diag = min(r8_diag)
+                min_r8_xy = min(r8_xy)
+                
+                #calculating the downhill slope to each of the lowest elevations
+                height_difference_diag = float(e_value) - float(min_r8_diag)
+                height_difference_xy = float(e_value) - float(min_r8_xy)
                 
                 #if height difference is negative it means we have a pit
-                if height_difference >= 0:     
-                    x_rad = math.atan(height_difference / cell_distance)
-                    x = math.degrees(x_rad)
+                if height_difference_diag or height_difference_xy >= 0:
+                    #calculate maximum perpendicular downhill slope
+                    xy_rad = math.atan(height_difference_xy / cell_distance)
+                    #convert radians to degrees and round to 1 decimal place
+                    xy = round(math.degrees(xy_rad),1)
+                    
+                    #calculate maximum diagonal downhill slope
+                    xd_rad = math.atan(height_difference_diag / (cell_distance*2)**0.5)
+                    #convert radians to degrees and round to 1 decimal place
+                    xd = round(math.degrees(xd_rad),1)     
+                    
+                    x = max([xy, xd]) #get largest downhil slope
+                    
+                #pits cancelled out to 0 gradient.
                 else:
-                    #pitRemove()
-                    #pitHighlight() 
-                    break
-                g_row.append(float(x))                
+                    x = 0    
+                #add each maximum downhill angle to row
+                g_row.append(float(x))           
                 i += 1 
+            #add each calculated row to gradient array    
             gradient.append(g_row)    
             n += 1
-        print("gradients calculated")
-
-
+            
+        print(gradient)
 
 
 class fileHandler():
     
     def tkFileAdd(in_raster, elevation):
-        print("loading file")
+        print("loading data from file")
+        #read in txt/csv elevation data
         in_raster = tkinter.filedialog.askopenfile(mode="r")
         reader = csv.reader(in_raster)
         for row in reader:
             rowlist = []
             for value in row:
+                #append each value to row
                 rowlist.append(float(value)) #float - matplotlib conversion error 
+            #append row to elevation    
             elevation.append(rowlist)       
         in_raster.close()
         print(elevation)
+        
+    def tkWebAdd(in_raster, elevation, webaddress):
+        print("loading data from webpage")
+        r = requests.get(
+            webaddress
+            )
+        content = r.text
+        soup = bs4.BeautifulSoup(content, 'html.parser')
+        x = soup.find_all()
+        print(x)
 
                                         
-                                        
     def txtWriter(gradient):
-        import csv
+        #write gradient array to .txt file
         print("compile data as .txt document")
         data = gradient
-        file = open('gradients.txt', 'w', newline='')
+        out_txt = tkinter.filedialog.asksaveasfilename() 
+        file = open(out_txt, 'w', newline='')
         writer = csv.writer(file, delimiter=',')
         for row in data:
             writer.writerow(row)
@@ -103,10 +141,11 @@ class fileHandler():
         
         
     def csvWriter(gradient):
+        #write gradient array to .csv file
         print("compile data as a .csv document")
-        import csv
         data = gradient
-        file = open('gradients.csv', 'w', newline='')
+        out_csv = tkinter.filedialog.asksaveasfilename() 
+        file = open(out_csv, 'w', newline='')
         writer = csv.writer(file, delimiter=',')
         for row in data:
             writer.writerow(row)
@@ -114,10 +153,11 @@ class fileHandler():
         
         
     def jsonWriter(gradient):
+        #write gradient array to .json file
         print("compile data as a .JSON document")
-        import json
         data = gradient
-        file = open('gradient.json', 'w')
+        out_json = tkinter.filedialog.asksaveasfilename() 
+        file = open(out_json, 'w')
         json.dump(data, file)
         file.close()
         
--- model.py
+++ model.py
@@ -4,60 +4,109 @@ Created on Wed May  6 19:20:43 2020
 
 @author: Robab
 """
-import bs4
-import requests
-import csv
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot
 import framework
 import tkinter
 
+
 elevation = []
 in_raster = 1
 gradient = []
-cell_distance = 25
+webaddress = 0
+
+
+def test():
+    print("dog, cat, fish, goat")
 
 
 def calculate():
+
     #external scripts directly run from tkinter run before tkinter.mainloop()
+        #set matplotlib window
+        #run gradient calculation
+        #show calculated gradients in matplotlib window
+    fig = matplotlib.pyplot.figure(figsize=(7, 7))
     framework.gradient.angle(elevation, cell_distance, gradient)
-    
+    matplotlib.pyplot.title("Gradient")
+    matplotlib.pyplot.imshow(gradient)
+
 
 def txtWriter():
     #external scripts directly run from tkinter run before tkinter.mainloop()
+    #pass gradient dataset to .txt file writer
     framework.fileHandler.txtWriter(gradient) 
 
   
 def csvWriter():
     #external scripts directly run from tkinter run before tkinter.mainloop()
+    #pass gradient dataset to .csv file writer    
     framework.fileHandler.csvWriter(gradient)
 
 
 def jsonWriter():
     #external scripts directly run from tkinter run before tkinter.mainloop()
+    #pass gradient dataset to .json file writer    
     framework.fileHandler.jsonWriter(gradient)
 
 
-def fileAdd():
+def fileImport():
+    
+    def enter_resolution():
+        #assign popup window entry to cell_distance global variable
+        global cell_distance
+        cd = v1.get()
+        cell_distance = int(cd)
+        print(cell_distance)
+        v1.quit()
+        #create matplotlib window and display imported elevation data
+        framework.fileHandler.tkFileAdd(in_raster, elevation)
+        fig = matplotlib.pyplot.figure(figsize=(7, 7))
+        matplotlib.pyplot.title("Elevation")
+        matplotlib.pyplot.imshow(elevation)    
+        
     #external scripts directly run from tkinter run before tkinter.mainloop()
-    framework.fileHandler.tkFileAdd(in_raster, elevation)
+    #create pop up window for user input of data resolution
+    root = tkinter.Tk()
+    tkinter.Label(root, text="input resolution (m)").grid(row=0)
+    v1 = tkinter.Entry(root)
+    v1.grid(row=0, column=1)
+    tkinter.Button(root, 
+                   text='Enter', 
+                   command=enter_resolution).grid(row=0, column=2)
+    tkinter.mainloop()
 
 
+#tkinter GUI setup
 root = tkinter.Tk()
 
+#set tkinter canvas extent
+canvas = tkinter.Canvas(root, bg="blue", height=300, width=500)
+canvas.pack()
+
+#add menu bar
 menu_bar = tkinter.Menu(root)
 root.config(menu=menu_bar)
 
+#assign canvas menus options
 file_menu = tkinter.Menu(menu_bar)
+file_import_menu = tkinter.Menu(menu_bar)
 file_export_menu = tkinter.Menu(menu_bar)
 analysis_menu = tkinter.Menu(menu_bar)
 
+#file >> import cascade options and commands
 menu_bar.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="Import", command=fileAdd)
+file_menu.add_cascade(label="Import", menu=file_import_menu)
+file_import_menu.add_command(label="Import from file", command=fileImport)
+
+#file >> export cascade options and commands
 file_menu.add_cascade(label="Export", menu=file_export_menu)
 file_export_menu.add_command(label="Export as TXT", command=txtWriter)
 file_export_menu.add_command(label="Export as CSV", command=csvWriter)
 file_export_menu.add_command(label="Export as JSON", command=jsonWriter)
 
+#analysis cascade options and commands
 menu_bar.add_cascade(label="Analysis", menu=analysis_menu)
 analysis_menu.add_command(label="Elevation to gradients", command=calculate)
 
